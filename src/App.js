2b93dde51a9fcce6e7d6f6c7bb4fbfeda65e4ee3
Finn Woelm <public@finnwoelm.com>
2020-01-20 22:04:15 +0100

    Set up React router: Admin & login page
    Create two routes, one public (login) and one private (admin).

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
+import AdminPage from './pages/AdminPage'
+import LoginPage from './pages/LoginPage'
+
+const PublicRoute = (props) => {
+  const user = false //netlifyIdentity.currentUser();
+
+  return user ? <Redirect to="/" /> : <Route {...props} />;
+};
+
+const PrivateRoute = (props) => {
+  const user = false // netlifyIdentity.currentUser();
+
+  return user ? <Route {...props} /> : <Redirect to={`/login${window.location.hash}`} />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute
+          exact path="/"
+          component={AdminPage}>
+        </PrivateRoute>
+        <PublicRoute
+          path="/login"
+          component={LoginPage}>
+          </PublicRoute>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
